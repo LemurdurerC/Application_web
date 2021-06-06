efed9983cbd2e1e99e9bcb6d545df330d40b3df0
LemurdurerC <claudomirkoda@gmail.com>
2021-06-06 02:36:20 +0200

    application billExtractor

---
+++ public/index.js
@@ -0,0 +1,36 @@
++
+    function ($) {
+        'use strict';
+
+        var uploadForm = document.getElementById('js-upload-form');
+
+        var startUpload = function (files) {
+            console.log(files)
+        }
+
+        uploadForm.addEventListener('submit', function (e) {
+            var uploadFiles = document.getElementById('js-upload-files').files;
+            e.preventDefault()
+            startUpload(uploadFiles)
+
+
+
+
+
+        })
+
+    }
+
+function loadFile(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    t = event.target.files[0];
+    let data = new FormData()
+    data.append('file', event.target.files[0])
+    console.debug("EVENT" + event.target.files[0])
+
+    fetch('http://localhost:8000', {
+        method: 'POST',
+        body: data
+    })
+}
